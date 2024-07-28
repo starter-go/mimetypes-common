3d4d54cf5466f852b0b66045a066b381a05252e6
xukun <xukun17@sina.com>
2024-07-28 23:30:46 +0800

    up to v0.0.1

--- module_t.go
+++ module_t.go
@@ -9,7 +9,7 @@ import (
 const (
 	theModuleName     = "github.com/starter-go/mimetypes-common"
 	theModuleVersion  = "v0.0.1"
-	theModuleRevision = 0
+	theModuleRevision = 1
 )
 
 ////////////////////////////////////////////////////////////////////////////////
